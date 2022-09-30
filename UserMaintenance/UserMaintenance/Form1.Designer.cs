6c8a71498629cb87b1b2f599219287165b695af6
sohazi <tamas.hardy@stud.uni-corvinus.hu>
2022-09-30 10:47:41 +0200

    Hatos

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -77,6 +77,7 @@ namespace UserMaintenance
             this.button1.TabIndex = 4;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listBox1
             // 
