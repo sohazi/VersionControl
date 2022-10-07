5861a3c67144e2de0002df35c3fd7702d4dc5b5b
sohazi <tamas.hardy@stud.uni-corvinus.hu>
2022-10-07 11:25:05 +0200

    nyolcadik

--- ExcelGood/ExcelGood/Form1.cs
+++ ExcelGood/ExcelGood/Form1.cs
@@ -103,9 +103,19 @@ namespace ExcelGood
              GetCell(2, 1),
              GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
 
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+           
 
 
-            }
+        }
 
             private string GetCell(int x, int y)
             {
