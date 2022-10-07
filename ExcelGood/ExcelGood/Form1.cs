7612aa49876dbfa466c2d1ebc93af32dc57f2783
sohazi <tamas.hardy@stud.uni-corvinus.hu>
2022-10-07 11:17:49 +0200

    Hetedik

--- ExcelGood/ExcelGood/Form1.cs
+++ ExcelGood/ExcelGood/Form1.cs
@@ -14,9 +14,9 @@ namespace ExcelGood
 {
     public partial class Form1 : Form
     {
-        Excel.Application xlApp; 
-        Excel.Workbook xlWB; 
-        Excel.Worksheet xlSheet; 
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
 
 
         RealEstateEntities context = new RealEstateEntities();
@@ -25,42 +25,37 @@ namespace ExcelGood
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
 
-            
         }
 
-    private void LoadData()
-    {
-        Flats = context.Flat.ToList();
-            
-    }
+        private void LoadData()
+        {
+            Flats = context.Flat.ToList();
+
+        }
 
         private void CreateExcel()
         {
             try
             {
-                
+
                 xlApp = new Excel.Application();
 
-               
                 xlWB = xlApp.Workbooks.Add(Missing.Value);
 
-                
                 xlSheet = xlWB.ActiveSheet;
 
-              
-                //CreateTable(); 
+                CreateTable();
 
-                
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
                 MessageBox.Show(errMsg, "Error");
 
-                
                 xlWB.Close(false, Type.Missing, Type.Missing);
                 xlApp.Quit();
                 xlWB = null;
@@ -69,8 +64,66 @@ namespace ExcelGood
 
         }
 
+        private void CreateTable()
+        {
+            string[] headers = new string[] {
+                 "Kód",
+                 "Eladó",
+                 "Oldal",
+                 "Kerület",
+                 "Lift",
+                 "Szobák száma",
+                 "Alapterület (m2)",
+                 "Ár (mFt)",
+                 "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, 1] = headers[0];
+            }
+
+                object[,] values = new object[Flats.Count, headers.Length];
+
+                int counter = 0;
+                foreach (Flat f in Flats)
+                {
+                    values[counter, 0] = f.Code;
+                    values[counter, 1] = f.Vendor;
+                    values[counter, 2] = f.Side;
+                    values[counter, 3] = f.District;
+                    values[counter, 4] = f.Elevator;
+                    values[counter, 5] = f.NumberOfRooms;
+                    values[counter, 6] = f.FloorArea;
+                    values[counter, 7] = f.Price;
+                    //values[counter, 8] = "";
+                    counter++;
+                }
+
+                xlSheet.get_Range(
+             GetCell(2, 1),
+             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
 
 
 
-    }
-}
+            }
+
+            private string GetCell(int x, int y)
+            {
+                string ExcelCoordinate = "";
+                int dividend = y;
+                int modulo;
+
+                while (dividend > 0)
+                {
+                    modulo = (dividend - 1) % 26;
+                    ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                    dividend = (int)((dividend - modulo) / 26);
+                }
+                ExcelCoordinate += x.ToString();
+
+                return ExcelCoordinate;
+            }
+
+
+        }
+    } 
