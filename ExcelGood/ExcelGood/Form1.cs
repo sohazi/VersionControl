07ba09a1cffe8238d578c8b8394101873f6904ae
sohazi <tamas.hardy@stud.uni-corvinus.hu>
2022-10-07 10:39:53 +0200

    Öt, hat

--- ExcelGood/ExcelGood/Form1.cs
+++ ExcelGood/ExcelGood/Form1.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace ExcelGood
 {
     public partial class Form1 : Form
     {
+        Excel.Application xlApp; 
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet; 
+
 
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
@@ -19,13 +25,51 @@ namespace ExcelGood
         {
             InitializeComponent();
             LoadData();
-           
+
+            
         }
 
-        private void LoadData()
+    private void LoadData()
+    {
+        Flats = context.Flat.ToList();
+            
+    }
+
+        private void CreateExcel()
         {
-            Flats = context.Flat.ToList();
+            try
+            {
+                
+                xlApp = new Excel.Application();
+
+               
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                
+                xlSheet = xlWB.ActiveSheet;
+
+              
+                //CreateTable(); 
+
+                
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) 
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+
         }
+
+
 
 
     }
