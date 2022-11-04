e1438282436581ea68ff412ea128178ffcc8e900
sohazi <tamas.hardy@stud.uni-corvinus.hu>
2022-11-04 10:08:32 +0100

    Labdagyár létrehozása

---
+++ Ajandek/Ajandek/Entities/BallFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ajandek.Entities
+{
+    class BallFactory
+    {
+        //public int MyProperty { get; set; }
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
