c2a8fb5f0b91da96a1e92f403c2ac63e5dfb9ee6
sohazi <tamas.hardy@stud.uni-corvinus.hu>
2022-11-04 11:01:18 +0100

    Autó létrehozása

---
+++ Ajandek/Ajandek/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using Ajandek.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Ajandek.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
