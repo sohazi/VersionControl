c97dc6b83a0cfe5938102764969105875a43c9aa
sohazi <tamas.hardy@stud.uni-corvinus.hu>
2022-11-04 10:53:34 +0100

    Játék absztrakció kezelése

---
+++ Ajandek/Ajandek/Abstractions/Toy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Ajandek.Abstractions
+{
+    public abstract class Toy:Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Height = 50;
+            Width = 50;
+            Paint += Ball_Paint;
+
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+        protected abstract void DrawImage(Graphics g);
+               
+
+        public virtual void MoveBall()
+        {
+            Left += 1;
+        }
+
+
+    }
+}
+
