5b1f3b119706c66b0dab02b385a5b363860f9eb0
sohazi <tamas.hardy@stud.uni-corvinus.hu>
2022-11-11 11:19:13 +0100

    utolso

--- Érték/Érték/Form1.cs
+++ Érték/Érték/Form1.cs
@@ -18,6 +18,7 @@ namespace Érték
 
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         List<Tick> Ticks = new List<Tick>();
+        List<decimal> Nyereségek = new List<decimal>();
         public Form1()
         {
             InitializeComponent();
@@ -25,7 +26,7 @@ namespace Érték
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
 
-            List<decimal> Nyereségek = new List<decimal>();
+            
             int intervalum = 30;
             DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
             DateTime záróDátum = new DateTime(2016, 12, 30);
@@ -75,17 +76,29 @@ namespace Érték
             SaveFileDialog sfd = new SaveFileDialog();
             if (sfd.ShowDialog() != DialogResult.OK) return;
 
+            sfd.InitialDirectory = Application.StartupPath; 
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv"; 
+            sfd.DefaultExt = "csv"; 
+            sfd.AddExtension = true;
+
             using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
-            {
-
-                foreach (var s in )
-                {
-
-                    sw.Write(s.Neptun);
+                
+            {                                                
+                    sw.Write("Időszak");
                     sw.Write(";");
-                    sw.Write(s.Name);
+                    sw.Write("Nyereség");
+                    sw.Write(";");
+                    int i = 0;
+                foreach (decimal s in Nyereségek)
+                {
+                    sw.Write(i.ToString());
+                    sw.Write(";");
+                    
 
                 }
+
+
+               
 
             }
 
