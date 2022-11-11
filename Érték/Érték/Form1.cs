2df641354ff8cc5ff645a70417261cfc39e96d67
sohazi <tamas.hardy@stud.uni-corvinus.hu>
2022-11-11 10:15:02 +0100

    Ötös

--- Érték/Érték/Form1.cs
+++ Érték/Érték/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Érték.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,12 +15,23 @@ namespace Érték
     {
         new PortfolioEntities context = new PortfolioEntities();
 
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         List<Tick> Ticks = new List<Tick>();
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
