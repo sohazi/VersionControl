6ece057642f5c110d0d575e4483468829ede1771
sohazi <tamas.hardy@stud.uni-corvinus.hu>
2022-12-02 10:53:12 +0100

    Szimuláció

--- none/none/Form1.cs
+++ none/none/Form1.cs
@@ -17,12 +17,34 @@ namespace none
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+
+        Random rng = new Random(1234);
         public Form1()
         {
             InitializeComponent();
             Population = GetPopulation(@"C:\Temp\nép.csv");
             BirthProbabilities = GetSzulVal(@"C:\Temp\születés.csv");
             DeathProbabilities = GetHalVal(@"C:\Temp\halál.csv");
+
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+                
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
+
         }
 
         public List<Person> GetPopulation(string csvpath)
