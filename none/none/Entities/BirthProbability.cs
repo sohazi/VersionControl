57ad09ed4132bf156499210de2a9c0577f490ede
sohazi <tamas.hardy@stud.uni-corvinus.hu>
2022-12-02 10:20:18 +0100

    osztályok létrehozása

---
+++ none/none/Entities/BirthProbability.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace none.Entities
+{
+    public class BirthProbability
+    {
+        public int kor { get; set; }
+
+        public int gyerekszám { get; set; }
+
+        public double valószínűség { get; set; }
+
+       
+    }
+}
